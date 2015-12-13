9a844eab2dcd84cd900b9705d0d4efca2d78b6f1
hobbes09 <sourin.sutradhar@gmail.com>
2015-12-13 17:54:09 +0530

    calling conversation activity

--- app/src/main/java/com/shopchat/consumer/adapters/InboxMessageListAdapter.java
+++ app/src/main/java/com/shopchat/consumer/adapters/InboxMessageListAdapter.java
@@ -1,15 +1,18 @@
 package com.shopchat.consumer.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.shopchat.consumer.R;
+import com.shopchat.consumer.activities.InboxMessageDetailActivity;
 import com.shopchat.consumer.activities.LandingActivity;
 import com.shopchat.consumer.models.entities.InboxMessageEntity;
 
@@ -76,6 +79,11 @@ public class InboxMessageListAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 Log.v("Inbox>>>", "You clicked :" + rowView.getTag());
+                Intent launchInboxMessageDetailActivity = new Intent(LandingActivity.mLandingActivityContext, InboxMessageDetailActivity.class);
+                Bundle questionInformation = new Bundle();
+                questionInformation.putString("QUESTION_ID", rowView.getTag().toString());
+                launchInboxMessageDetailActivity.putExtras(questionInformation);
+                LandingActivity.mLandingActivityContext.startActivity(launchInboxMessageDetailActivity);
             }
         });
         return rowView;
